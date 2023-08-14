1c46e93aa8b980987144b5a1fda21ecb1fd6d169
Anusha <Anusha@DESKTOP-O9VFEAF>
2023-08-14 13:49:09 -0700

    Update the inputs and outputs of the function to solve for SS & ODE. Importing parameters as input from an excel file. Importing inital values for state variables (yinit) as an input from the driver script. Added ATP and Parv buffers

--- SkelMuscleCa_SS.m
+++ SkelMuscleCa_SS.m
@@ -1,133 +1,62 @@
-function yInf = SkelMuscleCa_SS(lowATP, selParam)
+function [T,Y,yInf] = SkelMuscleCa_SS(tSpan,freq, lowATP, yinit,p)
 % [T,Y,yinit,param] = SkelMuscleCa_AK(argTimeSpan,argYinit,argParam)
 %
 % input:
+%     tSpan is a vector of start and stop times
 %     lowATP is true for low ATP conditions
-%     selParam is a vector of selected parameters to test
+%     p is a vector of selected parameters to test
+%     yinit is a vector of initial conditions for the state variables
 %
 % output:
+%     T is the vector of times
+%     Y is the vector of state variables
 %     yInf is the predicted SS values
 
 % Default Initial Conditions
 %
-
-yinit = [
-	0.0122; %4.743E-6;		% yinit(1) is the initial condition for 'SOCEProb'
-	1500.0;		% yinit(2) is the initial condition for 'c_SR'
-	0.9983;		% yinit(3) is the initial condition for 'h_K'
-	0.9091;		% yinit(4) is the initial condition for 'w_RyR'
-	0.9091;		% yinit(5) is the initial condition for 'w_DHPR'
-	-88.0;		% yinit(6) is the initial condition for 'Voltage_PM'
-	14700.0;		% yinit(7) is the initial condition for 'Na_i'
-	5830.0;		% yinit(8) is the initial condition for 'Cl_i'
-	0.1;		% yinit(9) is the initial condition for 'c_i'
-	0.003;		% yinit(10) is the initial condition for 'n'
-	0.0128;		% yinit(11) is the initial condition for 'm'
-	0.8051;		% yinit(12) is the initial condition for 'h'
-	0.8487;		% yinit(13) is the initial condition for 'S'
-	154500.0;		% yinit(14) is the initial condition for 'K_i'
-];
-
-%
 % Default Parameters
 %   constants are only those "Constants" from the Math Description that are just floating point numbers (no identifiers)
-%   note: constants of the form "A_init" are really initial conditions and are treated in "yinit"
-%
+%   note: constants of the form "A_init" are really initial conditions and are treated in "yinit" 
+% Parameters taken as input
 
-
-%% Parameters 
-param = [
-
-    288.0;          % param(1) is 'alpha_m0'
-    0.17;           % param(2) is 'K_PMCA'
-    1.0;            % param(3) is 'wUnit_RyR'
-    0.4;            % param(4) is 'delta'
-    13000.0;        % param(5) is 'K_mNa_NKX'
-    0.2;            % param(6) is 'f_RyR'
-    0.1E-6; %3.5420E-5;      % param(7) is 'J_NaK_NKX'
-    0.01;           % param(8) is 'tau_SOCEProb'
-    7.0;            % param(9) is 'K_alphan'
-    10.0;           % param(10) is 'K_alpham'
-    14.7;           % param(11) is 'K_alphah'
-    500.0;          % param(12) is 'c_ref'
-    1.0;            % param(13) is 'SUnit'
-    7.5;            % param(14) is 'A_hkinf'
-    1.0;            % param(15) is 'SOCEProbUnit'
-    0.1965;         % param(16) is 'g_Cl'
-    4380.0;         % param(17) is 'beta_h0'
-    1.0;            % param(18) is 'hUnit'
-    0.129;          % param(19) is 'g_NCX'
-    5.8;            % param(20) is 'A_Sinf'
-    8.04;           % param(21) is 'g_Na'
-    6.59;           % param(22) is 'Kmc_i_NCX'
-    8.1;            % param(23) is 'alpha_h0'
-    0.01;           % param(24) is 'C_PM'
-    1000.0;         % param(25) is 'K_mk_NKX'
-    1.0;            % param(26) is 'h_KUnit'
-    1.0;            % param(27) is 'nUnit'
-    1.0;            % param(28) is 'C_SRM'
-    1.57;           % param(29) is 'Q10NCX'
-    0.648;          % param(30) is 'g_K'
-    4.5;            % param(31) is 'K_RyR'
-    1.0;            % param(32) is 'wUnit_DHPR'
-    67.0;           % param(33) is 'beta_n0'
-    0.05;           % param(34) is 'Kdact_NCX'
-    10000.0;        % param(35) is 'S_i'
-    0.27;           % param(36) is 'nu_NCX'
-    40.0;           % param(37) is 'K_betan'
-    18.0;           % param(38) is 'K_betam'
-    9.0;            % param(39) is 'K_betah'
-    1380.0;         % param(40) is 'beta_m0'
-    1.0;            % param(41) is 'mUnit'
-    0.2;            % param(42) is 'f_DHPR'
-    4.5;            % param(43) is 'K_DHPR'
-    1000000.0;      % param(44) is 'K_S'
-    -20000.0;       % param(45) is 'device_totalCurrClampElectrode.F'
-    9.5E8;          % param(46) is 'K_K'
-    13.1;           % param(47) is 'alpha_n0'
-    0.32;           % param(48) is 'ksat_NCX'
-    150.0;          % param(49) is 'A_a'
-    0.111;          % param(50) is 'G_K'
-    1.0;            % param(51) is 'K_SERCA'
-    1.0;            % param(52) is 'tauUnit'
-    20;             % param(53) is 'R_fiber'
-    0.05;           % param(54) is 'volFraction_SR'
-    0.95;           % param(55) is 'volFraction_cyto'
-    0.003;          % param(56) is 'volFraction_TT'
-    0.05;           % param(57) is 'volFraction_SR'
-    100;            % param(58) is 'L_fiber'
-    0.01;           % param(59) is 'vol_SA_ratio'	
-];
-
-%
-% invoke the integrator
-%
-% options = odeset('RelTol',1e-8,'MaxStep',.001,'NonNegative',[1:5,7:14]);%,'OutputFcn',@odeplot);
-% % solve for SS Param
-% [~,SSParam] = f(0,yinit,param,yinit,freq,lowATP);
-% 
-% [T,Y] = ode15s(@f,timeSpan,yinit,options,param,yinit,freq,lowATP); %pass extra arguments at the end
-
-
-selIdx = [21,30,50]; % J_NaK, g_Cl, g_NCX, g_Na, g_K, G_K [[7,16,19,21,30,50]
-if size(selParam,1) == 1
-    selParam = selParam';
-end
-for i = 1: length(selParam)
-    param(selIdx(i)) = selParam(i).*param(selIdx(i));
-end 
 
 % solve for SS values
-% yInf = fsolve(@(y)f(y,param,lowATP), yinit);
 lb = zeros(14,1);
 lb(6) = -120;
 ub = 1e6 * ones(14,1);
 %ub(13) = 1;
-yInf = lsqnonlin(@(y)f(y,param,lowATP), yinit, lb, ub);
+yInf = lsqnonlin(@(y)f(y,p,lowATP), yinit, lb, ub);
 if any(yInf([1:5,7:14]) < 0)
     error('Variable should be non negative')
 end
 
+
+% selIdx = [21,30,48]; % J_NaK, g_Cl, g_NCX, g_Na, g_K, G_K [[7,16,19,21,30,50]
+% if size(p,1) == 1
+%     p = p';
+% end
+% param = [1,1,1];
+% for i = 1: length(p)
+%     param(selIdx(i)) = p(i).*param(selIdx(i));
+% end 
+
+% -------------------------------------------------------------------------
+
+timeSpan = [0.0 1.0];
+
+if nargin >= 1
+	if length(tSpan) > 0
+		%
+		% TimeSpan overridden by function arguments
+		%
+		timeSpan = tSpan;
+	end
+end
+%
+% invoke the integrator
+%
+options = odeset('RelTol',1e-8,'MaxStep',.001,'NonNegative',[1:5,7:14]);%,'OutputFcn',@odeplot);
+[T,Y] = ode15s(@f,timeSpan,yinit,options,param,yinit,freq,lowATP); %pass extra arguments at the end
 
 % -------------------------------------------------------
 % ode rate
@@ -148,67 +77,57 @@ function dydt = f(y, p, lowATP)
 	S = y(13);
 	K_i = y(14);
 	
-
+    
     % Parameters
-    alpha_m0 = p(1);
-    K_PMCA = p(2);
-    wUnit_RyR = p(3);
-    delta = p(4);
-    K_mNa_NKX = p(5);
-    f_RyR = p(6);
-    J_NaK_NKX = p(7);
-    tau_SOCEProb = p(8);
-    K_alphan = p(9);
-    K_alpham = p(10);
-    K_alphah = p(11);
-    c_ref = p(12);
-    SUnit = p(13);
-    A_hkinf = p(14);
-    SOCEProbUnit = p(15);
-    g_Cl = p(16);
-    beta_h0 = p(17);
-    hUnit = p(18);
-    g_NCX = p(19);
-    A_Sinf = p(20);
-    g_Na = p(21);
-    Kmc_i_NCX = p(22);
-    alpha_h0 = p(23);
-    C_PM = p(24);
-    K_mk_NKX = p(25);
-    h_KUnit = p(26);
-    nUnit = p(27);
-    C_SRM = p(28);
-    Q10NCX = p(29);
-    g_K = p(30);
-    K_RyR = p(31);
-    wUnit_DHPR = p(32);
-    beta_n0 = p(33);
-    Kdact_NCX = p(34);
-    S_i = p(35);
-    nu_NCX = p(36);
-    K_betan = p(37);
-    K_betam = p(38);
-    K_betah = p(39);
-    beta_m0 = p(40);
-    mUnit = p(41);
-    f_DHPR = p(42);
-    K_DHPR = p(43);
-    K_S = p(44);
-    device_totalCurrClampElectrode.F = p(45);
-    K_K = p(46);
-    alpha_n0 = p(47);
-    ksat_NCX = p(48);
-    A_a = p(49);
-    G_K = p(50);
-    K_SERCA = p(51);
-    tauUnit = p(52);
-    R_fiber = p(53);
-    volFraction_SR = p(54);
-    volFraction_cyto = p(55);
-    volFraction_TT = p(56);
-    volFraction_SR = p(57);
-    L_fiber = p(58);
-    vol_SA_ratio = p(59);
+    A_a = p(1);
+    A_hkinf = p(2);
+    A_Sinf = p(3);
+    alpha_h0 = p(4);
+    alpha_m0 = p(5);
+    alpha_n0 = p(6);
+    beta_h0 = p(7);
+    beta_m0 = p(8);
+    beta_n0 = p(9);
+    C_PM = p(10);
+    c_ref = p(11);
+    C_SRM = p(12);
+    delta = p(13);
+    device_totalCurrClampElectrode.F = p(14);
+    f_DHPR = p(15);
+    f_RyR = p(16);
+    g_Cl = p(17);
+    g_K = p(18);
+    G_K = p(19);
+    g_Na = p(20);
+    g_NCX = p(21);
+    J_NaK_NKX = p(22);
+    K_alphah = p(23);
+    K_alpham = p(24);
+    K_alphan = p(25);
+    K_betah = p(26);
+    K_betam = p(27);
+    K_betan = p(28);
+    K_DHPR = p(29);
+    K_K = p(30);
+    K_mk_NKX = p(31); 
+    K_mNa_NKX = p(32);
+    K_PMCA = p(33);
+    K_RyR = p(34);
+    K_S = p(35);
+    K_SERCA = p(36);
+    Kdact_NCX = p(37);
+    Kmc_i_NCX = p(38);
+    ksat_NCX = p(39);
+    nu_NCX = p(40);
+    Q10NCX = p(41);
+    R_fiber = p(42);
+    S_i = p(43);
+    tau_SOCEProb = p(44);
+    vol_SA_ratio = p(45);
+    volFraction_cyto = p(46);
+    volFraction_SR = p(47);
+    volFraction_TT = p(48);
+    
 
     % Constants
     c_EC_init_uM = 1300.0;
@@ -228,6 +147,17 @@ function dydt = f(y, p, lowATP)
     carrierValence_PMCA = 2.0;
     carrierValence_RyR = 1.0;
     carrierValence_SOCE = 2.0;
+
+    h_KUnit = 1.0; 
+    hUnit = 1.0;
+    L_fiber = 100;
+    mUnit = 1.0;
+    nUnit = 1.0;
+    SOCEProbUnit = 1.0;
+    SUnit = 1.0;
+    tauUnit = 1.0;
+    wUnit_DHPR = 1.0;
+    wUnit_RyR = 1.0;
 
     F = 96485.3321;
     PI = 3.141592653589793;
@@ -253,12 +183,10 @@ function dydt = f(y, p, lowATP)
     vol_SR = volFraction_SR * vol_Fiber ;
     SA_TT = volFraction_TT * vol_Fiber / vol_SA_ratio; 
     TTFrac = SA_TT / SA_PM;  
-    %SA_SRM = 1; %37698.0;
-
+   
     % Functions
 
-    %UnitFactor_uM_um3_molecules_neg_1 = KMOLE;
-	K_EC = K_EC_init_uM;
+    K_EC = K_EC_init_uM;
 	E_K_K_IR = (log((K_EC ./ K_i)) .* R .* T ./ F);
 	K_R = (K_EC .* exp(( - delta .* E_K_K_IR .* F ./ (R .* T))));
 	I_K_IR = ((G_K .* ((K_R ^ 2.0) ./ (K_K + (K_R ^ 2.0))) .* (1.0 - ((1.0 + ((K_S ./ ((S_i ^ 2.0) .* exp(((2.0 .* (1.0 - delta) .* Voltage_PM .* F) ./ (R .* T))))) .* (1.0 + ((K_R ^ 2.0) ./ K_K)))) ^  - 1.0)) .* (E_K_K_IR - Voltage_PM)) .* (1.0 + TTFrac));
@@ -268,8 +196,8 @@ function dydt = f(y, p, lowATP)
 	KmNa_i_NCX = (12.29 .* 1000.0);
 	volFactor = (vol_cyto ./ (PI .* 0.26));
 	nu_SERCA = (4875.0 .* volFactor);
-	LumpedJ_SERCA = (602.214179 .* nu_SERCA .* c_i ./ (K_SERCA + c_i)); 
-	I_Na = ((g_Na .* ((m ./ mUnit) ^ 3.0) .* (h ./ hUnit) .* (S ./ SUnit) .* (E_Na - Voltage_PM)) .* (1.0 + (0.1 .* TTFrac)));
+	LumpedJ_SERCA = (602.214179 * nu_SERCA * c_i ./ (K_SERCA + c_i)); 
+	I_Na = ((g_Na * ((m ./ mUnit) ^ 3.0) * (h ./ hUnit) * (S ./ SUnit) * (E_Na - Voltage_PM)) * (1.0 + (0.1 .* TTFrac)));
 	J_Na = ((I_Na ./ (carrierValence_Na .* F)) .* 1E09);
 	fPump_NKX_K = ((1.0 + (0.12 .* exp(( - 0.1 .* Voltage_PM .* F ./ (R .* T)))) + ((0.04 ./ 7.0) .* (exp((Na_EC ./ 67300.0)) - 1.0) .* exp(( - Voltage_PM .* F ./ (R .* T))))) ^  - 1.0);
 	I_NKX_K = ((2.0 .* (fPump_NKX_K .* F .* J_NaK_NKX ./ (((1.0 + (K_mk_NKX ./ K_EC)) ^ 2.0) .* ((1.0 + (K_mNa_NKX ./ Na_i)) ^ 3.0)))) .* (1.0 + (0.1 .* TTFrac)));
@@ -297,7 +225,7 @@ function dydt = f(y, p, lowATP)
 	Kmc_EC_NCX_N = (1.3 .* 1000.0);
 	s3_NCX_N = ((Kmc_i_NCX .* (Na_EC ^ 3.0) .* (1.0 + ((Na_i ./ KmNa_i_NCX) ^ 3.0))) + ((KmNa_EC_NCX_N ^ 3.0) .* c_i .* (1.0 + (c_i ./ Kmc_i_NCX))) + (Kmc_EC_NCX_N .* (Na_i ^ 3.0)) + ((Na_i ^ 3.0) .* c_EC) + ((Na_EC ^ 3.0) .* c_i));
 	I_NCX_N = ( - (3.0 .* (g_NCX .* (Q10NCX ^ Qcorr_NCX) .* Ka_NCX_N .* (s1_NCX_N - s2_NCX_N) ./ s3_NCX_N ./ (1.0 + (ksat_NCX .* exp(((nu_NCX - 1.0) .* Voltage_PM ./ (R .* T ./ F))))))) .* (1.0 + TTFrac));
-	A = (1.0 ./ (1.0 + exp(((Voltage_PM - V_a) ./ A_a))));
+	A = (1.0 / (1.0 + exp(((Voltage_PM - V_a) / A_a))));
 	Cl_EC = Cl_EC_init_uM;
 	E_Cl =  - (log((Cl_EC ./ Cl_i)) .* R .* T ./ F);
 	I_Cl = 0; % ((g_Cl .* (A ^ 4.0) .* (E_Cl - Voltage_PM)) .* (1.0 + (0.1 .* TTFrac)));
@@ -323,7 +251,6 @@ function dydt = f(y, p, lowATP)
 
     nu_leakSR = 0.2*volFactor;  %1.1338; %
     J_CaLeak_SR = 602.214179 * nu_leakSR * (c_SR - c_i);
-
 
 	alpha_h = (alpha_h0 .* exp(( - (Voltage_PM - V_h) ./ K_alphah)));
 	J_CaLeak_PM = ((I_CaLeak_PM ./ (carrierValence_CaLeak_PM .* F)) .* 1.0E09);
@@ -358,20 +285,42 @@ function dydt = f(y, p, lowATP)
 	J_NCX_C =  - ((I_NCX_C ./ (carrierValence_NCX_C .* F)) .* 1E09);
 	%KFlux_SRM_cyto = (SA_SRM ./ vol_cyto);
     %KFlux_SRM_cyto = (1/ vol_cyto);
-	%I_PM =  - device_totalCurrClampElectrode.F;
+	I_PM = 0; % - device_totalCurrClampElectrode.F;
 	J_PMCA =  - ((I_PMCA ./ (carrierValence_PMCA .* F)) .* 1E09);
 	device_PM.Capacitance = (C_PM .* SA_PM);
 	J_Cl = ((I_Cl ./ (carrierValence_Cl .* F)) .* 1E09);
+    
 
     %% Emmet - added code to VCell function
     % calcium buffering in the cytosol and SR
-    B_itot = 300;
-    K_iBuffer = 0.5;
+    % B_itot = 300;
+    % K_iBuffer = 0.5;
+    k_offATP = 0.01364;
+    k_onATP = 30;
+    K_iATP = k_offATP/k_onATP;
+    ATP_itot = 8000;
+    k_onParv = 0.0417;
+    k_offParv = 0.0005;
+    K_iParv = k_offParv/k_onParv;
+    Parv_itot = 1500;
+    f_i = 1/(1 + (K_iATP*ATP_itot/((K_iATP+c_i).^2)) + (K_iParv*Parv_itot/((K_iParv+c_i).^2)));
+    %f_i = 1/(1 + B_itot*K_iBuffer./((K_iBuffer+c_i).^2)); %Rapid equilibrium
+    CaParv = 0.258 * Parv_itot; %Fraction of Parv sites bound with Ca2+ at
+    %resting
+    CATP = 0*ATP_itot;
+    Parv = Parv_itot - CaParv; 
+    ATP = ATP_itot - CATP; 
+    %ATP = ATP_itot - CATP - MgATP;
+    %Parv = Parv_itot - CaParv - MgParv
+    
+    dCP = k_onParv*c_i*Parv - k_offParv*CaParv;
+
+    dCA = k_onATP*c_i*ATP - k_offATP*CATP;
+
     B_SRtot = 31000;
     K_SRBuffer = 500;
-    f_i = 1/(1 + B_itot*K_iBuffer./((K_iBuffer+c_i).^2));
     f_SR = 1/(1 + B_SRtot*K_SRBuffer./((K_SRBuffer+c_SR).^2));
-    I_PM = 0;
+    
 
     % switch lowATP condition on to test predictions in energy deficit (all
     % pumps go to half activity)
@@ -404,6 +353,8 @@ function dydt = f(y, p, lowATP)
 		J_r1;    % rate for h
 		J_r3;    % rate for S
 		KFlux_PM_cyto * ( J_K_IR - J_K_DR + J_NKX_K);    % rate for K_i
+        dCA; %Rate for ATP bound Ca2+
+        dCP;  % Rate for Parvalbumin bound Ca2+
 	];
     dydt = dydt ./ yinit;
 
