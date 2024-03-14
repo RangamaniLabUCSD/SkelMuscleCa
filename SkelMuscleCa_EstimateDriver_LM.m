c74ad86290997eeb2b260b90e97338fc6642647b
ank012 <125948438+ank012@users.noreply.github.com>
2024-03-14 10:53:14 -0700

    Added cases for penalty

--- SkelMuscleCa_EstimateDriver_LM.m
+++ SkelMuscleCa_EstimateDriver_LM.m
@@ -6,13 +6,13 @@ tSpan = [0 1];
             1500.0;		% yinit(2) is the initial condition for 'c_SR'
             0.9983;		% yinit(3) is the initial condition for 'h_K'
             0.9091;		% yinit(4) is the initial condition for 'w_RyR'            
-            -88.0;		% yinit(6) is the initial condition for 'Voltage_PM'
-            14700.0;	% yinit(7) is the initial condition for 'Na_i'
-            5830.0;		% yinit(8) is the initial condition for 'Cl_i'
-            0.1;		% yinit(9) is the initial condition for 'c_i'
-            0.003;		% yinit(10) is the initial condition for 'n'
-            0.0128;		% yinit(11) is the initial condition for 'm'
-            0.8051;		% yinit(12) is the initial condition for 'h'
+            -88.0;		% yinit(5) is the initial condition for 'Voltage_PM'
+            14700.0;	% yinit(6) is the initial condition for 'Na_i'
+            5830.0;		% yinit(7) is the initial condition for 'Cl_i'
+            0.1;		% yinit(8) is the initial condition for 'c_i'
+            0.003;		% yinit(9) is the initial condition for 'n'
+            0.0128;		% yinit(10) is the initial condition for 'm'
+            0.8051;		% yinit(11) is the initial condition for 'h'
             0.8487;		% yinit(13) is the initial condition for 'S'
             154500.0;	% yinit(14) is the initial condition for 'K_i'
             387;        % yinit(15) is the initial condition for 'CaParv'
@@ -21,7 +21,7 @@ tSpan = [0 1];
             ];
  
 % Parameter values
-param = importdata('InputParam1.xlsx');
+param = importdata('InputParam.xlsx');
 p =  param.data; 
 
 % ClampCurrent = p(1) ;K_S = p(2) ;delta = p(3) ;beta_m0 = p(4) ;K_betam = p(5) ;alpha_m0 = p(6) ;K_alpham = p(7) ;K_RyR = p(8) ;f_RyR = p(9) ;
@@ -34,7 +34,5 @@ ub = 2*ones(length(p),1);
 timer1 = tic;
 [pSol_LM,fval,exitflag] = SkelMuscleCa_paramEst_LM(tSpan,lb, ub, yinit, p);
 toc(timer1)
-
-saveas(gcf,'BestFnva_2_16_1_LM.jpg')
-
-
+filename_fig = "BestFnva"+ date + ".jpg";
+saveas(gcf,filename_fig)
--- SkelMuscleCa_paramEst_LM.m
+++ SkelMuscleCa_paramEst_LM.m
@@ -12,19 +12,29 @@
 
 function [pSol,fval,exitflag] = SkelMuscleCa_paramEst_LM(~,lb,ub,yinit,p_est)
 
-psOptions = optimoptions('particleswarm','SwarmSize',100,'UseParallel',true,'HybridFcn',@fmincon,...
-    'PlotFcn','pswplotbestf','Display','iter','MaxStallIterations',50); 
+psOptions = optimoptions('particleswarm','UseParallel',true,'HybridFcn',@fmincon,...
+    'PlotFcn','pswplotbestf','Display','iter','MaxStallIterations',100); %'SwarmSize',100, 
 
 numParam = length(lb);
 pVec = ones(1,numParam); %41
-% load LM_PSO_2_16_1.mat pSol_LM
-% pVec = pSol_LM;
-pToObj(pVec)
+% load PSO_11-Mar-2024_2.mat pSol
+% pVec = pSol;
+% error_i = pToObj(pVec);
+%%
+% pVec = 0.8 + (1.25 - 0.8)* rand(100,numParam);
+% for a = 1:100
+%     error(a) = pToObj(pVec(a,:));
+% end
+%%
+% pVec = [1.13221318715608	0.910784829890607	1.08766990881102	0.960520363703168	0.881220949480694	1.14980749044848	1.01784366722258	0.832650430164826	1.13204434984506	1.21134064952321	0.853989752515383	1.02556941419551	1.04505055423832	1.24069003552003	1.20312237401022	1.05626370998520	1.21757747171939	0.906544295531512	0.811685340553057	0.948058482316822	1.18105151245671	1.00575732420527	0.872659170917175	1.24424664541645	1.20403751216216	1.15221082903596	1.11655793288450	0.878720899125972	1.06486411347693	1.14367568901613	0.890078146487194	1.24850588834879	1.24985485041859	0.811471290427414	1.03705854781936	1.02675718342494	0.917570025970154	1.14125840581366	1.10821470759528	0.890444308368329	1.17040333644423];
+% pToObj(pVec)
 delete(gcp('nocreate'))
 parpool(50)
 [pSol,fval,exitflag] = particleswarm(@pToObj,numParam,lb,ub,psOptions);
 pToObj(pSol)
-save('LM_PSO_2_16_1.mat')
+filename = "PSO_" + date +"_2.mat";
+save(filename);
+
 
     function objVal = pToObj(pVec) % Obj function should be scalar
 
@@ -34,113 +44,164 @@ save('LM_PSO_2_16_1.mat')
         InterpExpt = cell(1,9);
         Expt_t = cell(1,9);
         InterpComp = cell(1,9);
+        CompV = cell(1,5);
+        CompC = cell(1,5);
         %yinf_ratio = zeros(9,17);
 
         load Exptdata.mat Expt
         %Expt = {[R_t R_C],[R_MP_t R_MP_C] [HB_t HB_C], [H_t H_C],[HB_MP_t HB_MP_C], [K_t K_V], [B_t B_V] , [M_t M_V], [W_t W_V], [MJ_T MJ_V]};
         StartTimer = tic;
         freq = [100, 100, 67, 67,67, 60, 60, 60,60];
+        expt_title = ["Rincon","Rincon", "Baylor & Hollingworth", "Hollingworth", "Baylor & Hollingworth", "Yonemura","Bibollet", "Miranda","Wallinga"];
         param = p_est(:) .* pVec(:);
-        tSS = [0 1000];
+        tSS = 0:1000;
+
+        expt_n = [1 5 7 8]; % 1:9; % [1 8];%
 
         %Interpolating experimental values
-        for m = 1 %:9
+        for m_index = 1 :length(expt_n) %:9
+            m = expt_n(m_index);
             T_max(m) = max(Expt{m}(:,1))/1000;
             Expt_t{m} = Expt{m}(:,1)/1000;
+            if m < 6
+                Expt{m}(:,2) = Expt{m}(:,2) + 0.1;
+            end
             InterpExpt{m} = interp1(Expt_t{m},Expt{m}(:,2),0:0.0001:T_max(m));
         end
 
         penaltyVal = 100000;
         count = 0;
-        for n = 1 %:9         
+        for n_index = 1 :length(expt_n) %:9 
+            n = expt_n(n_index);
             %% Colmpute SS yinit with LM algorithm.
-            initialGuess = yinit;
-            options = optimoptions('lsqnonlin', 'Algorithm', 'levenberg-marquardt', 'Display', 'off', 'MaxIter', 10000); %,'UseParallel',true);
-            lb1 = 0.5 * initialGuess;
-            ub1 = 2 * initialGuess;
-            lb1(5) = 2 * initialGuess(5);
-            ub1(5) = 0.5 * initialGuess(5);
+            %initialGuess = yinit;
+            %options = optimoptions('lsqnonlin', 'Algorithm', 'levenberg-marquardt', 'Display', 'off', 'MaxIter', 10000); %,'UseParallel',true);
+            %lb1 = 0.5 * initialGuess;
+            %ub1 = 2 * initialGuess;
+            %lb1(5) = 2 * initialGuess(5);
+            %ub1(5) = 0.5 * initialGuess(5);
 
-            [y_LM] = lsqnonlin(@(initialGuess) objectivefn(initialGuess,n,param),initialGuess, lb1, ub1, options);
-            if any(isnan(y_LM))
-                objVal = penaltyVal;
-                return
-            end
+            %[y_LM] = lsqnonlin(@(initialGuess) objectivefn(initialGuess,n,param),initialGuess, lb1, ub1, options);
+            %if any(isnan(y_LM))
+            %    objVal = penaltyVal;
+            %    return
+            %end
              
             %% Compute SS with ode15s         
-            [~,ySS] = SkelMuscleCa1_SS(tSS,0, 0, yinit, param,StartTimer,n);
-
+            [TimeSS,ySS] = SkelMuscleCa1_SS(tSS,0, 0, yinit, param,StartTimer,n);
+            if size(ySS,1) < length(tSS)
+                objVal = penaltyVal;
+                count = count+1;
+                return
+            end
             if any(isnan(ySS))
                 objVal = penaltyVal;
                 count = count+1;    
                 return
             end
-            yinf = ySS(end,:);            
+            yinf = ySS(end,:);   
+             
+            % figure
+            % plot(TimeSS,ySS(:,6),'b','LineWidth',2)
+            % hold on
+            % plot(TimeSS,ySS(:,7),'r','LineWidth',2)
+            % hold on
+            % plot(TimeSS,ySS(:,13),'g','LineWidth',2)
+            % xlabel('Time (s)')
+            % legend('[Na^+]', '[Cl^-]', '[K^+]');
+            % hold off
+            % figure
+            % plot(TimeSS,ySS(:,5),'b','LineWidth',2)
+            % hold on
+            % plot(TimeSS,ySS(:,8),'r','LineWidth',2)
+            % hold off
+            % xlabel('Time (s)')
+            % legend('V_{SL}', '[Ca^{2+}]');
+
+        
        
           
             %% Calculate Dynamics
-            t = [0 T_max(n)];
+            t = 0:0.0001:T_max(n);
             [Time,y] = SkelMuscleCa1_SS(t,freq(n), 0, yinf, param,StartTimer,n);
             %[Time,y] = SkelMuscleCa1_SS([0 t(n)],freq(n), 0, yinit, p,tic,n);
-            if size(y,1) < 2
+            if size(y,1) < length(t) 
                 objVal = penaltyVal;
                 count = count+1;
                 return
             end
             if n <= 5
-                Comp = y(:,8); % Calcium Calculations
+                InterpComp{n} = y(:,8) ; % Calcium Calculations
+                CompV{n} = y(:,5) ;
             elseif n > 5
-                Comp = y(:,5); % Voltage Calculations
+                InterpComp{n} = y(:,5); % Voltage Calculations
+                CompC{n} = y(:,8);
             end
-            InterpComp{n} = interp1(Time,Comp,0:0.0001:T_max(n));            
+            %InterpComp{n} = interp1(Time,Comp,0:0.0001:T_max(n));            
             %yinit = yinf;
         end
 
-        %%
-        % Plots       
+        %% Plots   
         % figure
-        % for i = 1
+        % subplot(1,2,1)
+        % plot(0:0.0001:T_max(1),CompV{1},'b','LineWidth',2)
+        % subplot(1,2,2)
+        % plot(0:0.0001:T_max(5),CompV{5},'b','LineWidth',2)
+        % %plot (Time,InterpComp{1},'r')
+        % hold off
+        % xlabel('Time (s)');
+        % title('V_{SL} for expt 1 vs 5'); %', num2str(i));
+
+        % figure
+        % for index = 1:2 %
+        %     i = expt_n(index);
+        %     subplot(2,2,index)
         %     plot(0:0.0001:T_max(i),InterpComp{i},'b','LineWidth',2)
         %     hold on
         %     plot(0:0.0001:T_max(i),InterpExpt{i},'r','LineWidth',2)
         %     xlabel('Time (s)');
         %     legend('Computational','Experimental')
-        %     title('Computational vs expt \Delta[Ca^{2+}] concentration for expt - Rincon',i); % num2str(i));
+        %     title('Computational vs expt \Delta[Ca^{2+}] concentration for expt - '+ expt_title(i)); % num2str(i));
         %     ylabel('\Delta[Ca^{2+}] Concentration (uM)');
         %     fontsize(15,"points")
         % end
-
-        % figure
-        % for i = 6:9        
-        %     subplot(2,2,i-5)            
+        % 
+        % 
+        % % figure
+        % for index = 3:4 
+        %     i = expt_n(index); %:9
+        %     subplot(2,2,index)
         %     plot(0:0.0001:T_max(i),InterpComp{i},'b','LineWidth',2)
         %     hold on
         %     plot(0:0.0001:T_max(i),InterpExpt{i},'r','LineWidth',2)
         %     xlabel('Time (s)');
         %     legend('Computational','Experimental')
         %     ylabel('V_{PM} (mV)');
-        %     title('Computational vs expt V_{PM} for expt %d',i); %', num2str(i));
+        %     title('Computational vs expt V_{PM} for expt - '+ expt_title(i)); %');
         %     fontsize(15,"points")
-        % end 
-        
+        % end
+
         %% Objective Value Calc       
         delta = cell(1,9);
         sum_delta = zeros(1,9);
+        Error = cell(1,9);
+       
+        for j_index = 1 :length(expt_n) %:9
+            j = expt_n(j_index);
+            weight = length(InterpExpt{j}) ;
+            sigma_C = (0.05 * InterpExpt{j}); %
+            sigma_V = 5 ;
+            delta{j} = InterpComp{j}' - InterpExpt{j};
+            if j < 6               
+                Error{j} = ((delta{j} ./ sigma_C ) .^ 2 )./ weight; %     (((InterpComp{j}' - InterpExpt{j})./sigma_C).^2)./weight ;
+            elseif j > 5
+                Error{j} = ((delta{j} ./ sigma_V ) .^ 2 )./ weight; % (((InterpComp{j}' - InterpExpt{j}) ./ sigma_V).^2) ./ weight;
+            end
+             sum_delta(j) = sum(Error{j});
 
-        for j = 1 %:9
-             weight = length(InterpExpt{j});
-             sigma_C = (0.1 + (0.05 * InterpExpt{j})).^2;
-             if j < 6
-                 delta{j} = ((InterpComp{j} - InterpExpt{j}).^2);%/ (weight * sigma_C) ;
-            else
-                if j > 5
-                    delta{j} = ((InterpComp{j} - InterpExpt{j}).^2)/(weight* 25) ;
-                end
-             end
-                sum_delta(j) = sum(delta{j});
-           
-         end 
+        end
         
         objVal = sum(sum_delta);
     end
 end
+
