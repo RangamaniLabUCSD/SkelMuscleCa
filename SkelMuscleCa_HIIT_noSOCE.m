dfb6957ae542ea087926a9f012b6aeb68709511b
ank012 <125948438+ank012@users.noreply.github.com>
2024-06-04 15:25:20 -0700

    Adding exercise files

---
+++ SkelMuscleCa_HIIT_noSOCE.m
@@ -0,0 +1,41 @@
+clc
+clear
+%delete(gcp('nocreate'))
+
+load PSO_25-Apr-2024.mat pSol yinit p
+p = p(:) .* pSol(:);
+
+%% Expt : 
+%1,2 - Cont Stim (Control & no SOCE)
+%3,4 - Wei-Lapierre (Control & noSOCE)
+%5,6 - Resistance (Control & noSOCE)
+%%7,8 - HIIT (COntrol & noSOCE)
+
+f_HIIT = 60:10:180;
+l_HIIT = length(f_HIIT);
+
+Time_noSOCE8 = cell(l_HIIT,1);
+Ca_noSOCE8 = cell(l_HIIT,1);
+CaSR_noSOCE8 = cell(l_HIIT,1);
+SOCEProb_noSOCE8 = cell(l_HIIT,1);
+Flux_noSOCE8 = cell(l_HIIT,1);
+Current_noSOCE8 = cell(l_HIIT,1);
+%% Steady State calculation -----------------------------------------------
+[~,ySS_noSOCE] = SkelMuscleCa_dydt([0 1000],0, 0, yinit, p, tic, 2);  
+yinf_noSOCE = ySS_noSOCE(end,:);
+
+parpool(15)
+% HIIT Dynamics calculation ---------------------------------------------------
+for i = 1:l_HIIT
+    freq = f_HIIT(i);    
+    expt = 8;
+    [Time_noSOCE,Y_noSOCE, ~, fluxes_noSOCE,currents_noSOCE] = SkelMuscleCa_dydt([0 60], freq, 0, yinf_noSOCE, p, tic, expt);   % Dynamics computation
+    Time_noSOCE8{i} = Time_noSOCE;
+    Ca_noSOCE8{i} = Y_noSOCE(:,8);
+    CaSR_noSOCE8{i} = Y_noSOCE(:,2);
+    SOCEProb_noSOCE8 = Y_noSOCE(:,1);
+    Flux_noSOCE8{i} = fluxes_noSOCE;
+    Current_noSOCE8{i} = currents_noSOCE;
+end
+
+save('HIIT_noSOCE8_5-9.mat');
---
+++ SkelMuscleCa_Resistance_noSOCE.m
@@ -0,0 +1,43 @@
+clc
+
+delete(gcp('nocreate'))
+
+load PSO_25-Apr-2024.mat pSol yinit p
+p = p(:) .* pSol(:);
+
+% Expt : 
+%1,2 - Cont Stim (Control & no SOCE)
+%3,4 - Wei-Lapierre (Control & noSOCE)
+%5,6 - Resistance (Control & noSOCE)
+%%7,8 - HIIT (COntrol & noSOCE)
+
+f_Resistance = 10:5:50;
+l_Resistance = length(f_Resistance);
+
+Time_noSOCE6 = cell(l_Resistance,1);
+Ca_noSOCE6 = cell(l_Resistance,1);
+CaSR_noSOCE6 = cell(l_Resistance,1);
+SOCEProb_noSOCE6 = cell(l_Resistance,1);
+Flux_noSOCE6 = cell(l_Resistance,1);
+Current_noSOCE6 = cell(l_Resistance,1);
+
+%% Steady State calculation -----------------------------------------------
+[~,ySS_noSOCE] = SkelMuscleCa_dydt([0 1000],0, 0, yinit, p, tic, 2);  
+yinf_noSOCE = ySS_noSOCE(end,:);
+
+%%
+% Resistance Dynamics calculation ---------------------------------------------------
+parpool(10)
+parfor i = 1:l_Resistance
+    freq = f_Resistance(i);    
+    expt = 6;
+    [Time_noSOCE,Y_noSOCE, ~, fluxes_noSOCE,currents_noSOCE] = SkelMuscleCa_dydt([0 420], freq, 0, yinf_noSOCE, p, tic, expt);   % Dynamics computation
+    Time_noSOCE6{i} = Time_noSOCE;
+    Ca_noSOCE6{i} = Y_noSOCE(:,8);
+    CaSR_noSOCE6{i} = Y_noSOCE(:,2);
+    SOCEProb_noSOCE6 = Y_noSOCE(:,1);
+    Flux_noSOCE6{i} = fluxes_noSOCE;
+    Current_noSOCE6{i} = currents_noSOCE;
+end
+
+save('Resistance_noSOCE6_5-9.mat');
